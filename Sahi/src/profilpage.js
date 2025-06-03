d7dbb91caa2329304b174fefe68a83c6b18ec7f9
İbrahim Bayır <94011558+ibrahimbayir@users.noreply.github.com>
2025-06-03 14:02:28 +0300

    Update profilpage.js

--- Sahi/src/profilpage.js
+++ Sahi/src/profilpage.js
@@ -55,7 +55,7 @@ function Profilepage() {
     };
 
     try {
-      const res = await fetch("http://localhost:3030/api/user/update", {
+      const res = await fetch("https://sahibindenkarsilastir.up.railway.app/api/user/update", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(updatedUser),
